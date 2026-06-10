5b892f795b9fc2e127970e40dc7454e62fdf4ffe
yjhhe <hyj500555@gmail.com>
2026-06-10 19:28:59 +0800

    实现轮播图渲染

--- lib/components/Home/Hmslider.dart
+++ lib/components/Home/Hmslider.dart
@@ -1,20 +1,46 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/viewmodels/home.dart';
 
 class Hmslider extends StatefulWidget {
-  const Hmslider({super.key});
+  final List<BannerItem> bannerList;
+  Hmslider({Key? key, required this.bannerList}):super(key:key);
 
   @override
   State<Hmslider> createState() => _HmsliderState();
 }
 
 class _HmsliderState extends State<Hmslider> {
+
+  Widget _getSlider(){
+
+    final double screenWidth = MediaQuery.of(context).size.width;
+//轮播图插件
+    return CarouselSlider(
+      items: List.generate(widget.bannerList.length, (int index) {
+        return Image.network(
+          widget.bannerList[index].imgUrl,
+          fit: BoxFit.cover,
+          width: screenWidth,
+          );
+    }), 
+    options: CarouselOptions(
+      autoPlay: true,
+      viewportFraction: 1)
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+
+    return Stack(
+      children: [_getSlider()
+      ],);
+    /* Container(
       height:300,
       color: Colors.blue,
       alignment: Alignment.center,
       child: Text("轮播图",style: TextStyle(color: Colors.white,fontSize: 20)),
-    );
+    ); */
   }
 }
--- lib/pages/Home/index.dart
+++ lib/pages/Home/index.dart
@@ -4,6 +4,7 @@ import 'package:flutter_application_1/components/Home/HmHot.dart';
 import 'package:flutter_application_1/components/Home/HmMoreList.dart';
 import 'package:flutter_application_1/components/Home/HmSuggestion.dart';
 import 'package:flutter_application_1/components/Home/Hmslider.dart';
+import 'package:flutter_application_1/viewmodels/home.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -13,10 +14,24 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+
+  final List<BannerItem> _bannerList = [
+    BannerItem(
+      id:"1", 
+      imgUrl: "https://n.sinaimg.cn/sinakd20112/384/w2048h1536/20220731/5c76-18753651605f5006fe7cc156f19584e7.jpg"
+      ),
+    BannerItem(
+      id: "2", 
+      imgUrl: "https://y3.ifengimg.com/a/2014_52/2633f87e648cb10.jpg"),
+    BannerItem(
+      id: "3", 
+      imgUrl: "https://n.sinaimg.cn/sinakd20112/384/w2048h1536/20220731/b460-d29aff0e856054d1df870fd0768f0e3b.jpg"),
+  ];
+
   List<Widget> _getScrollChildern(){
     return [
       //轮播图组件
-      SliverToBoxAdapter(child: Hmslider()),
+      SliverToBoxAdapter(child: Hmslider(bannerList: _bannerList)),
       //分类组件 SizedBox一个简单的空隙组件
       SliverToBoxAdapter(child: SizedBox(height: 10)),
       //分类
